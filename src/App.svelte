66f161193d516023a0d45d5739c9eb1b834ca7eb
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-11-08 17:03:49 -0600

    added an input in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,11 +5,20 @@
 <style>
 	h1 {
 		color: purple;
-	}
+  }
+
+  label {
+    display: inline-block;
+  }
 </style>
 
 <svelte:head>
   <title>My SSR Demo</title>
 </svelte:head>
+
+<div>
+  <label>Name</label>
+  <input bind:value={name} />
+</div>
 
 <h1>Hello {name}!</h1>
